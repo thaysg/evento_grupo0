7ecff6060e3b0b87f3fae5ad3f8d415a6770cba7
Ailton Dos Santos <f.futuretechcode@gmail.com>
2021-10-16 18:15:13 -0300

    Colocando as cores principais.

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color primaryColor = Colors.purple;
+  static const Color primaryColor = Color(0xffFF8100);
+  static const Color primaryDark = Color(0xff262525);
 }
